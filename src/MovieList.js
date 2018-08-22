8cfa4ac92162e0ceddb5e5eaa87a47bed2c46975
HoratiuL <bonsay5@yahoo.com>
2018-08-22 20:25:56 +0300

    Add movie card
    Create movie card

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
-
+import MovieItem from './MovieItem';
+import styles from './MovieList.css';
 
 function MovieList() {
     return (
-        <Button variant="fab" color="secondary" aria-label="Add">
-        <AddIcon />
-        </Button>
+        <div className="movie-list">
+            <div className="movies-container">
+                <MovieItem title="Deadpool 2" 
+                year="2012"
+                description="This is a small synopsis" />
+            </div>
+            <Button variant="fab" color="secondary" aria-label="Add">
+                <AddIcon />
+            </Button>
+        </div>
     )
 }
 
