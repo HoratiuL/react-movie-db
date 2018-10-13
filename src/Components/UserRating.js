226282e3c1cdb812d6703797cb89683bf0987790
HoratiuL <bonsay5@yahoo.com>
2018-10-13 11:23:45 +0300

    UserRating

--- src/Components/UserRating.js
+++ src/Components/UserRating.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
 import Icon from "@material-ui/core/Icon";
+import IconButton from "@material-ui/core/IconButton";
 
 const Star = props => (
-  <span>
+  <span
+    onClick={props.onClick}
+    onMouseEnter={props.onMouseEnter}
+    onMouseLeave={props.onMouseLeave}
+  >
     <Icon>{props.selected ? "star_border" : "star"}</Icon>
   </span>
 );
@@ -21,13 +26,22 @@ export default class UserRating extends Component {
     this.state = { stars };
   }
 
+  onMouseEnter(ev, el) {}
+  onMouseLeave(ev, el) {}
+
   render() {
     return (
       <div>
         User rating:
         <p>
           {this.state.stars.map((e, i) => (
-            <Star key={i} index={i} />
+            <Star
+              key={i}
+              index={i}
+              onMouseEnter={() => console.log("mouse enter")}
+              onMouseLeave={() => console.log("mouse leave")}
+              onClick={e => console.log(e.id)}
+            />
           ))}
         </p>
       </div>
